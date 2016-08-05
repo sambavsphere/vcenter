2ec083457e3514e115137ebf47154eafdbae9924
sambavsphere <sambavsphere@gmail.com>
2016-08-05 16:18:49 +0530

    sdfdsf

--- vcentermonitoring/flask_app/app.py
+++ vcentermonitoring/flask_app/app.py
@@ -1,10 +1,20 @@
 from flask import Flask,render_template
 from sqlit import get_hosts_info,get_vms_info
 app = Flask(__name__)
+import json
+import pdb
 
 @app.route('/')
-def hello_world():
-    allhsots = get_hosts_info()
-    print allhsots,'$$$$$$$$$$$$$$$$$$$$$$$4'
-    return render_template('home.html',hosts=allhsots)
+def hosts():
+    hosts = get_hosts_info()
+    header = hosts.columns.tolist()
+    data = hosts.to_json(orient='records')
+    return render_template('home.html',header= json.dumps(header), data = data, am="hosts")
+
+@app.route('/vms')
+def vms():
+    hosts = get_vms_info()
+    header = hosts.columns.tolist()
+    data = hosts.to_json(orient='records')
+    return render_template('home.html',header= json.dumps(header), data = data, am="vms")
 app.run()
